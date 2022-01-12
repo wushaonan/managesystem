8e2dc6dda20e272349456fe99ad25516660e9f29
wushaonan <wushaonan@xiahongshu.com>
2022-01-12 15:19:23 +0800

    框架搭建

---
+++ src/main/java/com/sn/manageservice/ManageServiceApplication.java
@@ -0,0 +1,16 @@
+package com.sn.manageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author user
+ */
+@SpringBootApplication
+public class ManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sn/manageservice/ManageserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sn.manageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
